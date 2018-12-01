42464b32b5a8f7d77c97552040fa8c57fbeaf3db
FHilding <tony.fat@gmail.com>
2018-12-01 16:49:16 +0100

    Day 1 completed

---
+++ Day1.py
@@ -0,0 +1,18 @@
+myfile = open('day1data.txt', 'r')
+contents = myfile.read().strip().split()
+myfile.close()
+
+print(sum([int(x) for x in contents]))
+start = 0
+matchedFrequencies = set([start])
+matched = False
+while matched != True:
+    for line in contents:
+        start += int(line)
+        if start in matchedFrequencies:
+            matched = True
+            break
+        else:
+            matchedFrequencies.add(start)
+
+print(start)
